1b86a6b582bc3342f356e6b2dd53d9fb1b73592d
realingy <754037927@qq.com>
2019-10-19 15:05:40 +0800

    双目视觉第一次commit

---
+++ 20191014/01_Capture/capture.cpp
@@ -0,0 +1,25 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace cv;
+
+void main()
+{
+	VideoCapture cap;
+	cap.open(1); //打开摄像头
+	if (!cap.isOpened()) {	//如果视频不能正常打开则返回
+		std::cout << "打开摄像头错误！\n";
+		return;
+	}
+
+	Mat frame;
+	while (1)
+	{
+		cap >> frame; //等价于cap.read(frame);
+		if (frame.empty()) //如果某帧为空则退出循环
+			break;
+		imshow("video", frame);
+		waitKey(20); //每帧延时 20 毫秒
+	}
+	cap.release(); //释放资源
+}
+

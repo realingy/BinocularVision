5ef495d55b127e74ab8b37a827a2be5ad943ab91
realingy <754037927@qq.com>
2019-10-20 22:40:34 +0800

    优化代码

--- vs2017/PCLTest/pcltest.cpp
+++ vs2017/PCLTest/pcltest.cpp
@@ -1,13 +1,13 @@
 #define  _SCL_SECURE_NO_WARNINGS
 #include <pcl/visualization/cloud_viewer.h>
 #include <pcl/io/io.h>
-#include <pcl/io/pcd_io.h> //pcd 读写类相关的头文件。
+#include <pcl/io/pcd_io.h> //pcd 读写类相关的头文件
 #include <pcl/io/ply_io.h>
-#include <pcl/point_types.h> //PCL中支持的点类型头文件。
+#include <pcl/point_types.h> //PCL中支持的点类型头文件
 #include <iostream>
 
 int user_data;
-using std::cout;
+using namespace std;
 
 void viewerOneOff(pcl::visualization::PCLVisualizer & viewer)
 {
@@ -16,10 +16,10 @@ void viewerOneOff(pcl::visualization::PCLVisualizer & viewer)
 
 int main()
 {
-#if 1
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
 
 	char strfilepath[256] = "rabbit.pcd";
+	// char strfilepath[256] = "pcd/frame_00000.pcd";
 	if (-1 == pcl::io::loadPCDFile(strfilepath, *cloud)) {
 		cout << "error input!" << endl;
 		return -1;
@@ -30,8 +30,14 @@ int main()
 
 	viewer.showCloud(cloud);
 	viewer.runOnVisualizationThreadOnce(viewerOneOff);
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << (float)cloud->points[i].x << " " << (float)cloud->points[i].y << " " << (float)cloud->points[i].z << endl;
+	}
+
 	system("pause");
-#endif
+
 	return 0;
 }
 
